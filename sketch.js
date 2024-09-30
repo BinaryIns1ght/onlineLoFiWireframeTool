467ca7b970a66e979d5818de1ab4560a00cb6543
BinaryIns1ght <79720466+BinaryIns1ght@users.noreply.github.com>
2024-09-30 15:42:23 -0700

    Progress 1
    Set up the some of the visual aspects of the project and started on p5.js work

--- sketch.js
+++ sketch.js
@@ -1,7 +1,33 @@
+let bgColor, canvasButton, circleX;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1500, 900);
+  
+  bgColor = createColorPicker('gray');
+  bgColor.position(200, 1480);
+  bgColor.size(100, 30);
+  bgColor.draggable();
+
+  circleX = createInput();
+  circleX.position(150, 1585);
+  circleX.size(100, 20);
+  circleX.draggable();
+  
+  canvasButton = createButton('Save Canvas', 100, 1500);
+  canvasButton.mousePressed(canvasSaved);
 }
 
 function draw() {
-  background(220);
+  background(bgColor.color());
+  text('Backgroud color: ' + bgColor.value(), 10, 875);  
 }
+
+function canvasSaved() {
+  save();
+}
+
+// class dragging {
+//   constructor() {
+
+//   }
+// }
