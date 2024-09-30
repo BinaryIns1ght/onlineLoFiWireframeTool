ba800dfe41eae126eb6fef4daa25553a029c4b55
BinaryIns1ght <79720466+BinaryIns1ght@users.noreply.github.com>
2024-09-30 16:18:54 -0700

    Progress 2
    Made small changes to the stylization and formatting of the site.

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let bgColor, canvasButton, circleX;
+let bgColor, canvasButton, circleX, circleY, circleS;
 
 function setup() {
   createCanvas(1500, 900);
@@ -12,7 +12,20 @@ function setup() {
   circleX.position(150, 1585);
   circleX.size(100, 20);
   circleX.draggable();
-  
+  let cX = circleX.value();
+
+  circleY = createInput();
+  circleY.position(415, 1585);
+  circleY.size(100, 20);
+  circleY.draggable();
+  let cY= circleY.value();
+
+  circleS = createInput();
+  circleS.position(648, 1585);
+  circleS.size(100, 20);
+  circleS.draggable();
+  let cS = circleS.value();
+
   canvasButton = createButton('Save Canvas', 100, 1500);
   canvasButton.mousePressed(canvasSaved);
 }
@@ -24,6 +37,10 @@ function draw() {
 
 function canvasSaved() {
   save();
+}
+
+function makeCircle() {
+
 }
 
 // class dragging {
