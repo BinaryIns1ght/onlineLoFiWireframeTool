80b3bdc51a717b76a92bd083406d96effd8bc2e9
BinaryIns1ght <79720466+BinaryIns1ght@users.noreply.github.com>
2024-09-30 21:49:08 -0700

    Progress 4
    Redone a good amount of the sketch.js

--- sketch.js
+++ sketch.js
@@ -1,86 +1,22 @@
-let bgColor, canvasButton, circleX, circleY, circleS, circleC, circleB, makingCircles, bgCheck, colorValue;
+let bgR, bgG, bgB, bg1, bg2, bg3, bg4;
 
 function setup() {
   createCanvas(1500, 900);
 
-  bgCheck = createCheckbox(' Transparent Background?');
-  bgCheck.position(350, 1480)
+  bgR = createSlider(0, 255, 255);
+  bgR.position(100, 1420);
 
-  colorValue = createCheckbox(' Remove Background Color Text?');
-  colorValue.position(700, 1480)
-  
-  bgColor = createColorPicker('gray');
-  bgColor.position(200, 1480);
-  bgColor.size(100, 30);
-  bgColor.draggable();
+  bgG = createSlider(0, 255, 255);
+  bgG.position(300, 1420);
 
-  circleX = createInput();
-  circleX.position(150, 1560);
-  circleX.size(100, 20);
-  circleX.draggable();
-  let cX = circleX.value();
+  bgB = createSlider(0, 255, 255);
+  bgB.position(500, 1420);
 
-  circleY = createInput();
-  circleY.position(415, 1560);
-  circleY.size(100, 20);
-  circleY.draggable();
-  let cY= circleY.value();
 
-  circleS = createInput();
-  circleS.position(648, 1560);
-  circleS.size(100, 20);
-  circleS.draggable();
-  let cS = circleS.value();
-
-  circleC = createColorPicker('white');
-  circleC.position(900, 1555);
-  circleC.size(100, 30);
-  circleC.draggable();
-  let cC = circleC.color();
-
-  circleB = createColorPicker('white');
-  circleB.position(1200, 1555);
-  circleB.size(100, 30);
-  let cB = circleB.value();
-
-  makingCircles = createButton('Make Your Circle', 100, 1500);
-  makingCircles.mousePressed();
-  makingCircles.position(1350, 1560);
-
-  canvasButton = createButton('Save Canvas', 100, 1500);
-  canvasButton.mousePressed(canvasSaved);
 }
 
 function draw() {
-  if (bgCheck.checked()) {
-    clear();
-  } 
-  else {
-    background(bgColor.color());
-  }
-  
-  if (colorValue.checked()) {
-    text(' ', 0, 0);
-  }
-  else{
-    text('Backgroud color: ' + bgColor.color(), 10, 875);  
-  }
+  let RGBA = color(bgR.value(), bgG.value(), bgB.value());
+  background(RGBA);
 }
 
-function canvasSaved() {
-  save('mySketch.png');
-}
-
-class Circle {
-  constructor(cX, cY, cS, cC, cB){
-    this.x = cX,
-    this.y = cY,
-    this.size = cS,
-    this.color = cC,
-    this.border = cB
-  }
-
-  display() {
-    
-  }
-}
